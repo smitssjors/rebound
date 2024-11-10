ba1c2c9200c3b0af4bf977c00cd23ed43a2d022e
Sjors Smits <smits.sjors@gmail.com>
2024-11-10 15:36:27 +0100

    improve preformance

--- init.sql
+++ init.sql
@@ -1,8 +1,9 @@
--- PRAGMA journal_mode=WAL;
--- PRAGMA synchronous=NORMAL;
--- PRAGMA busy_timeout=5000;
--- PRAGMA cache_size=2000;
--- PRAGMA mmap_size=134217728; -- 128MB
+PRAGMA journal_mode=WAL;
+PRAGMA synchronous=NORMAL;
+PRAGMA busy_timeout=5000;
+PRAGMA cache_size=2000;
+PRAGMA mmap_size=134217728; -- 128MB
+PRAGMA temp_store=MEMORY;
 
 CREATE TABLE IF NOT EXISTS messages (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -12,3 +13,4 @@ CREATE TABLE IF NOT EXISTS messages (
 	ttr INTEGER NOT NULL,
 	body TEXT NOT NULL
 ) STRICT;
+CREATE INDEX IF NOT EXISTS messages_queue_locked_until ON messages (queue, locked_until); 
---
+++ store_test.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"context"
+	"database/sql"
+	"testing"
+	"time"
+)
+
+func BenchmarkPut(b *testing.B) {
+	store, db, err := openTestStore(b)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_, err := store.Put(context.Background(), "test", 0, 0, 2*time.Minute, "hello")
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
+
+func BenchmarkReserve(b *testing.B) {
+	store, db, err := openTestStore(b)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	for range 1000 {
+		_, err = store.Put(context.Background(), "test", 0, 0, 2*time.Minute, "hello")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			_, err := store.Reserve(context.Background(), "test")
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
+
+func BenchmarkDelete(b *testing.B) {
+	store, db, err := openTestStore(b)
+	if err != nil {
+		b.Fatal(err)
+	}
+	defer db.Close()
+	for range 1000 {
+		_, err = store.Put(context.Background(), "test", 0, 0, 2*time.Minute, "hello")
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+
+	b.ResetTimer()
+
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			err := store.Delete(context.Background(), "test", 1)
+			if err != nil {
+				b.Fatal(err)
+			}
+		}
+	})
+}
+
+func openTestStore(b *testing.B) (Store, *sql.DB, error) {
+	tempdir := b.TempDir()
+	db, err := sql.Open("sqlite3", tempdir+"/rebound.db")
+	if err != nil {
+		return nil, nil, err
+	}
+
+	if _, err := db.Exec(initQuery); err != nil {
+		return nil, nil, err
+	}
+	return NewStore(db), db, nil
+}
